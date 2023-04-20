db46f83bd39183ee4e8ed687a3ddee7f0d17d498
gulyasmir <gulyasmir@yandex.ru>
2023-04-20 13:22:59 +0300

    fix

--- index.js
+++ index.js
@@ -20,16 +20,17 @@ http
             }          
           });
         }
-      });
-      res.setHeader('Access-Control-Allow-Headers', '*');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write("<h1>Загрузились</h1>");
-      console.log('res', res)
-      res.end();
 
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write("<h1>Загрузились</h1>");
+        console.log('res', res)
+        res.end();
+
+      });
 
     } else { // download/...
      
