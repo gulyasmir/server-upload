aaec82e544a27fe86381a0dda70d6833732eff07
gulyasmir <gulyasmir@yandex.ru>
2023-04-20 13:48:24 +0300

    fix

--- index.js
+++ index.js
@@ -44,26 +44,11 @@ http
      
       fs.readFile(item, function (err, content) {
         if (err) {
-          let mapRUS = "./uploads/setting-mapRUS.json";
-          fs.access(mapRUS, fs.constants.F_OK, (err) => {
-            //check that we can access  the file
-            console.log(`${item} ${err ? "does not exist" : "exists"}`);
-          });
-          fs.readFile(mapRUS, function (err, content) {
-            if (err) {
-              let img = "./uploads/setting-mapRUS.json";
-              res.setHeader('Access-Control-Allow-Headers', '*');
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              res.writeHead(404, { "Content-type": "text/html" });
-              res.end("<h1>No such file</h1>");
-            } else {
-              //specify the content type in the response will be an image
-              res.setHeader('Access-Control-Allow-Headers', '*');
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              res.writeHead(200, { "Content-type": "text/json" });
-              res.end(content);
-            }
-          });
+        
+          res.setHeader('Access-Control-Allow-Headers', '*');
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.writeHead(404, { "Content-type": "text/html" });
+          res.end("<h1>No such file</h1>");
         } else {
           //specify the content type in the response will be an image
           res.setHeader('Access-Control-Allow-Headers', '*');
